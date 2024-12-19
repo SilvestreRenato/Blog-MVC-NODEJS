48e974ac96d60f13ee93bc6727a6945e2d536acf
Renato Silvestre <silvestrerenato17@gmail.com>
2024-12-19 19:01:05 -0400

    Define routes file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const path = require("node:path")
+const router = require("./routes")
 const app = express()
 
 // Configuração EJS
@@ -11,6 +12,8 @@ app.use(express.static("public"))
 
 // Configuração para ler dados da requisição
 app.use(express.urlencoded({ extended: true }))
+
+app.use(router)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Servidor iniciado!\nRodando na porta http://localhost:${PORT}/`))
