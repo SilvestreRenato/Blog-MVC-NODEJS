7363132e830bb1029386f15c5a687bd6c9fda4e6
Renato Silvestre <silvestrerenato17@gmail.com>
2024-12-19 18:54:50 -0400

    Configure EJS, static files, and request data handling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const express = require("express")
+const path = require("node:path")
 const app = express()
+
+// Configuração EJS
+app.set("view engine", "ejs")
+app.set("view", path.join(__dirname, "views"))
+
+// Configuração de arquivos estaticos
+app.use(express.static("public"))
+
+// Configuração para ler dados da requisição
+app.use(express.urlencoded({ extended: true }))
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Servidor iniciado!\nRodando na porta http://localhost:${PORT}/`))
